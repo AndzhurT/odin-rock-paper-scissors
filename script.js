85e004f847ec09e8ec35d884973409aede71d8c2
AndzhurT <andjur38@gmail.com>
2024-05-15 22:20:37 -0400

    Add human choice and computer choice functions
    Add implementation of human choice and computer choice functions

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 
 
-console.log("Hello World")
+
+// function declaration - global 
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    let randomChoice = Math.floor(Math.random() * choices.length); 
+    
+    return choices[randomChoice];
+}
+
+console.log(getComputerChoice())
+
+
+function getHumanChoice() {
+    humanChoice = prompt("What would you choose? Rock, Paper or Scissors?");
+    return humanChoice;
+}
+
+console.log(getHumanChoice())
+
+let humanScore = 0;
+let computerScore = 0;
